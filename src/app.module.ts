ab41d37813b9f2a37b03d089509e5485d45298e4
marcumo <macuevam@gmail.com>
2022-10-07 00:46:58 -0500

    eliminado products  y documentados los schemas

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { ProductsModule } from './products/products.module';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [UsersModule, ProductsModule, DatabaseModule],
+  imports: [UsersModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
